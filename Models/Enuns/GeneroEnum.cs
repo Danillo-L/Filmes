bdc44c0db9d1931a18fa28be23b6aeea157a0c6b
Danillo <danillo.lacerda77@gmail.com>
2024-12-04 22:45:58 -0300

    Criar atributo descrição e classes Editora e Ator

--- Models/Enuns/GeneroEnum.cs
+++ Models/Enuns/GeneroEnum.cs
@@ -2,14 +2,14 @@ namespace Filmes.Models.Enuns
 {
     public enum GeneroEnum
     {
-        Ação,
-        Aventura,
-        Terror,
-        FiccaoCientifica,
-        Thriller,
-        Romance,
-        Comedia,
-        Documentario,
-        Drama,
+        Ação = 1,
+        Aventura = 2,
+        Terror = 3,
+        FiccaoCientifica = 4,
+        Thriller = 5,
+        Romance = 6,
+        Comedia = 7,
+        Documentario = 8,
+        Drama = 9
     }
 }
